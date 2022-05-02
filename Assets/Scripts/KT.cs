372a22f3c78906f891eb4a93a9bcf02b3fb0e25e
rangddang <doma051029@naver.com>
2022-05-03 08:14:16 +0900

    +Roof
    +Roof
    +anim
    +body(?)

--- Assets/Scripts/KT.cs
+++ Assets/Scripts/KT.cs
@@ -6,18 +6,14 @@ public class KT : KTEye
 	{
 		base.Update();
 
-		MoveKT();
+		TurnObject();
 	}
 
 	protected override void TurnObject()
 	{
 		Vector3 direction = (target.position - transform.position);
 		Quaternion rotation = Quaternion.LookRotation(direction);
+		rotation.eulerAngles += new Vector3(-90f, 0, 0);
 		transform.rotation = Quaternion.Lerp(transform.rotation, rotation, turnSpeed * Time.deltaTime);
-	}
-
-	private void MoveKT()
-	{
-		//if()
 	}
 }
